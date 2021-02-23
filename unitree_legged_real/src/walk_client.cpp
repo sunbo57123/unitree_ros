39575d2e58d1169e7e1059088e982facc0e0dab8
sun bo <sunbo32@huawei.com>
2021-02-23 19:24:21 +0800

    change pub/sub methods

--- unitree_legged_real/src/walk_client.cpp
+++ unitree_legged_real/src/walk_client.cpp
@@ -8,32 +8,38 @@
 #include "rclcpp/rclcpp.hpp"
 #include "unitree_legged_msgs/msg/walk.hpp"
 
-int main(int argc, char* argv[]){
-    rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("walk_pub");
-    auto walk_publisher = node->create_publisher<unitree_legged_msgs::msg::Walk>("topic", 10);
-    rclcpp::WallRate loop_rate(2);
-    auto msg = std::make_shared<unitree_legged_msgs::msg::Walk>();
-    auto highcmd = std::make_shared<unitree_legged_msgs::msg::HighCmd>();
-    while (rclcpp::ok()){
-        msg->time = 2;
-        highcmd->forwardspeed = 1.00f;
-        highcmd->sidespeed = 0.0f;
-        highcmd->rotatespeed = 0.0f;
-        highcmd->bodyheight = 0.0f;
+using namespace std::chrono_literals;
 
-        highcmd->mode = 0;
-        highcmd->roll = 0;
-        highcmd->pitch = 0;
-        highcmd->yaw = 0;
-
-        msg->highcmd =*highcmd;
-
-        walk_publisher->publish(*msg);
-        RCLCPP_INFO(node->get_logger(), "send time %d", msg->time);
-        rclcpp::spin_some(node);
-        loop_rate.sleep();
+class PubNode: public rclcpp::Node
+{
+public:
+    PubNode():Node("pub_node"), count_(0){
+        walk_pub = this->create_publisher<unitree_legged_msgs::msg::Walk>("walk", 10);
+        timer_ = this->create_wall_timer(500ms, std::bind(&PubNode::pub_callback, this));
     }
+
+private:
+    void pub_callback(){
+        unitree_legged_msgs::msg::Walk msg;
+        msg.mode = 1;
+        msg.forwardspeed = 0.0f;
+        msg.rotatespeed = 0.0f;
+        msg.sidespeed = 0.0f;
+        msg.time = 6;
+        RCLCPP_INFO(this->get_logger(), "sending");
+        walk_pub->publish(msg);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<unitree_legged_msgs::msg::Walk>::SharedPtr walk_pub;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<PubNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 
 }
--- unitree_legged_real/src/walk_instruction.cpp
+++ unitree_legged_real/src/walk_instruction.cpp
@@ -1,53 +1,68 @@
 
 // Created by sun on 2021/2/19.
 
-#include <rclcpp/rclcpp.hpp>
-#include <pthread.h>
-#include <time.h>
-#include <string>
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
-#include "std_msgs/msg/string.hpp"
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
 #include "unitree_legged_msgs/msg/walk.hpp"
+#include "unitree_legged_msgs/msg/high_cmd.hpp"
+#include "unitree_legged_msgs/msg/high_state.hpp"
 #include "unitree_legged_sdk/unitree_legged_sdk.h"
-#include "convert.h"
 
-using namespace UNITREE_LEGGED_SDK;
-
-void walk_callback(const unitree_legged_msgs::msg::Walk::SharedPtr msg){
-    std::cout<< "received : " << msg->time << std::endl;
-//    walk related instructions are highlevel cmds.
-    UDP udp = UDP(HIGHLEVEL);
-    HighCmd cmd = {0};
-    udp.InitCmdData(cmd);
-    int time = msg->time;
-    cmd.forwardSpeed = msg->highcmd.forwardspeed;
-    cmd.sideSpeed = msg->highcmd.sidespeed;
-    cmd.rotateSpeed = msg->highcmd.rotatespeed;
-    cmd.bodyHeight = msg->highcmd.bodyheight;
-    cmd.mode = msg->highcmd.mode;
-    cmd.roll = msg->highcmd.roll;
-    cmd.pitch = msg->highcmd.pitch;
-    cmd.yaw = msg->highcmd.yaw;
-
-    udp.SetSend(cmd);
-    udp.Send();
-
-}
-
-int main(int argc, char* argv[])
+class WalkNode : public rclcpp::Node
 {
-    InitEnvironment();
+public:
+    WalkNode():Node("walk_node"), udp(UNITREE_LEGGED_SDK::HIGHLEVEL),safe(UNITREE_LEGGED_SDK::LeggedType::A1), high_cmd({0}), high_state({0})
+    {
+        walk_sub = this->create_subscription<unitree_legged_msgs::msg::Walk>(
+                "walk",
+                10,
+                std::bind(&WalkNode::walk_callback,
+                          this,
+                          std::placeholders::_1));
+        udp.InitCmdData(high_cmd);
+    }
+    void UDPRecv(){
+        udp.Recv();
+    }
+    void UDPSend(){
+        udp.Send();
+    }
+
+    UNITREE_LEGGED_SDK::Safety safe;
+    UNITREE_LEGGED_SDK::UDP udp;
+    unitree_legged_msgs::msg::Walk walk_cmd;
+    UNITREE_LEGGED_SDK::HighCmd high_cmd;
+    UNITREE_LEGGED_SDK::HighState high_state;
+    float dt = 0.002;
+
+private:
+    void walk_callback(const unitree_legged_msgs::msg::Walk::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(), "received : %d", msg->time);
+        if (msg->time >0){
+            if (msg->mode == 1){
+                high_cmd.mode = 2;
+                sleep(30);
+            }
+        }
+        udp.SetSend(high_cmd);
+    }
+    rclcpp::Subscription<unitree_legged_msgs::msg::Walk>::SharedPtr walk_sub;
+};
+
+int main(int argc, char* argv[]){
+
+    std::cout << "start" << std::endl;
+
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("walk_ros");
-    auto walk_sub = node->create_subscription<unitree_legged_msgs::msg::Walk>("topic", 10, walk_callback);
+    auto node = std::make_shared<WalkNode>();
+    UNITREE_LEGGED_SDK::InitEnvironment();
+    UNITREE_LEGGED_SDK::LoopFunc loop_udpRecv("UDP receive", node->dt, 3 , boost::bind(&WalkNode::UDPRecv,&node));
+    loop_udpRecv.start();
+    UNITREE_LEGGED_SDK::LoopFunc loop_udpSend("UDP send", node->dt, 3 , boost::bind(&WalkNode::UDPSend,&node));
+    loop_udpSend.start();
+
     rclcpp::spin(node);
-
-
-//    UNITREE_LEGGED_SDK::LeggedType rname = UNITREE_LEGGED_SDK::LeggedType::A1;
-//
-//    UNITREE_LEGGED_SDK::InitEnvironment();
-//    UNITREE_LEGGED_SDK::LCM roslcm(UNITREE_LEGGED_SDK::HIGHLEVEL);
-//    mainhelper<UNITREE_LEGGED_SDK::HighCmd, UNITREE_LEGGED_SDK::HighState, UNITREE_LEGGED_SDK::LCM>(argc, argv, roslcm);
+    rclcpp::shutdown();
     return 0;
 }
